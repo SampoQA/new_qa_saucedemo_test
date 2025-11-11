f669bf4380539ebdfae46499a5c65afebbed8cfd
SampoQA <linnahuo@gmail.com>
2025-11-11 20:10:11 +0300

    add print, re

--- tests/test_add_to_chart.py
+++ tests/test_add_to_chart.py
@@ -3,6 +3,7 @@ from playwright.sync_api import Page, expect
 
 def test_login(page: Page):
     page.goto("https://www.saucedemo.com/")  #go to site
-    expect(page).to_have_title("Swag Labs")
+    expect(page).to_have_title(re.compile("Swag Labs"))
+    print("The password entry page contains the words Swag Labs")
 
 
